cc1f5360d7717aa658a5ca3fc7e2787085947815
Nguyen Duy Hoang <ndhoang22@clc.fitus.edu.vn>
2024-11-26 23:39:47 +0700

    remove dotenv dependency and update Firebase database URL in configuration

--- webSrc/config/firebase.js
+++ webSrc/config/firebase.js
@@ -1,7 +1,7 @@
 var admin = require("firebase-admin");
 
 var serviceAccount = require("../phy00007-smart-hamgiuxe-22326-firebase-adminsdk-7vqqz-4a0a6b40fc.json");
-const databaseURL = process.env.FIREBASE_DATABASE_URL;
+const databaseURL = "https://phy00007-smart-hamgiuxe-22326-default-rtdb.firebaseio.com";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
